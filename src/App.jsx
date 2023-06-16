e0410fc9a3590fde0874adf405952baea8b98bb6
unknown <matheus.duarte42@gmail.com>
2023-06-16 10:58:14 -0300

    estrutura do formulario finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import GlobalStyle from './styles/global'
 import Header from './components/Header/Header'
 import Resume from './components/Resume/Resume'
+import Form from './components/Form/Form'
 
 const App = () => {
   return (
     <>
       <Header />
       <Resume />
+      <Form />
       <GlobalStyle />
     </>
   )
